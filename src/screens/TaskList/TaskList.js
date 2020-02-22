938a851774c2c06f016239e6508d2c677e53b084
William P. Pinto <william.pinto@lambda3.com.br>
2020-02-22 20:02:10 -0300

    Adding first styles and backgound image to task list

--- src/screens/TaskList/TaskList.js
+++ src/screens/TaskList/TaskList.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground} from 'react-native';
+
+import styles from './styles';
+import todayImage from '../../../assets/imgs/today.jpg';
 
 export default class TaskList extends Component {
   render() {
     return (
-      <View>
-        <Text>TaskList</Text>
+      <View style={styles.container}>
+        <ImageBackground source={todayImage} style={styles.background} />
+        <Text style={styles.listContainer}>TaskList</Text>
       </View>
     );
   }
---
+++ src/screens/TaskList/styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 3,
+  },
+  listContainer: {
+    flex: 7,
+  },
+});
