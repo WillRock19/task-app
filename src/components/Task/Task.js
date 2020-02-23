d69c8e2114deb933c6e69ba9563efbb9d139d064
William P. Pinto <william.pinto@lambda3.com.br>
2020-02-23 13:09:33 -0300

    Adding an Icon to the Task component; creating checked elements to show if task is completed

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
+function getCheckView(doneAt) {
+  if (doneAt != null) {
+    return (
+      <View style={[styles.checkItem, styles.done]}>
+        <Icon name="check" size={19} color="#FFF" />
+      </View>
+    );
+  }
+  return <View style={[styles.checkItem, styles.pending]} />;
+}
+
 export default props => {
   return (
-    <View>
-      <Text>{props.description}</Text>
-      <Text>{props.estimatedAt + ''}</Text>
-      <Text>{props.doneAt + ''}</Text>
+    <View style={styles.container}>
+      <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+      <View>
+        <Text>{props.description}</Text>
+        <Text>{props.estimatedAt + ''}</Text>
+      </View>
     </View>
   );
 };
--- src/components/Task/styles.js
+++ src/components/Task/styles.js
@@ -1,5 +1,30 @@
 import {StyleSheet} from 'react-native';
+import commonStyles from './../../common/styles';
 
 export default StyleSheet.create({
-  teste: {},
+  container: {
+    alignItems: 'center', //defines the align of the cross-axis from flex-box (its a column, since flexDirection [main-axis] is row)
+    flexDirection: 'row',
+    borderColor: '#AAA',
+    borderBottomWidth: 1,
+  },
+  checkContainer: {
+    alignItems: 'center', //defines the horizontal alignment, since the flex-direction (main-axis), by default, is 'column', the cross-axis will be 'row'
+    justifyContent: 'center',
+    width: '20%',
+  },
+  checkItem: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+  },
+  pending: {
+    borderWidth: 1,
+    borderColor: '#555',
+  },
+  done: {
+    alignItems: 'center',
+    backgroundColor: '#4D7031',
+    justifyContent: 'center',
+  },
 });
