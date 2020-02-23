7dd1cfce0b4db10272602f3e7ef878d2da5bd3ba
William P. Pinto <william.pinto@lambda3.com.br>
2020-02-23 12:28:33 -0300

    Created object with common styles; adding styles and showing date on TaskList

--- src/screens/TaskList/TaskList.js
+++ src/screens/TaskList/TaskList.js
@@ -4,11 +4,23 @@ import {View, Text, ImageBackground} from 'react-native';
 import styles from './styles';
 import todayImage from '../../../assets/imgs/today.jpg';
 
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
 export default class TaskList extends Component {
   render() {
+    const today = moment()
+      .locale('pt-br')
+      .format('ddd, D [de] MMMM');
+
     return (
       <View style={styles.container}>
-        <ImageBackground source={todayImage} style={styles.background} />
+        <ImageBackground source={todayImage} style={styles.background}>
+          <View style={styles.titleBar}>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>{today}</Text>
+          </View>
+        </ImageBackground>
         <Text style={styles.listContainer}>TaskList</Text>
       </View>
     );
--- src/screens/TaskList/styles.js
+++ src/screens/TaskList/styles.js
@@ -1,4 +1,5 @@
 import {StyleSheet} from 'react-native';
+import commonStyles from '../../common/styles';
 
 export default StyleSheet.create({
   container: {
@@ -10,4 +11,22 @@ export default StyleSheet.create({
   listContainer: {
     flex: 7,
   },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    color: commonStyles.colors.secondary,
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  subtitle: {
+    color: commonStyles.colors.secondary,
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
+  }
 });
